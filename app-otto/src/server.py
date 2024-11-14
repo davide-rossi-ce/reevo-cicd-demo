aeaaef7cef1dab00526f3704e7159f6c7b583de7
Davide Rossi <davide.rossi@reevo.it>
2024-11-14 04:18:06 +0100

    add several indipendent app instances

---
+++ app-otto/src/server.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import json
+import math
+from socketserver import ThreadingMixIn
+
+hostName = "0.0.0.0"
+serverPort = int(os.environ.get('SERVER_PORT', '8000'))
+developerName = os.environ.get('DEVELOPER_NAME', "Davide")
+status = { "code": 200, "healthy": "OK"}
+
+
+class Handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    match self.path:
+      case "/":
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        content = open('index.html', 'rb').read()
+        self.wfile.write(content)
+      case "/index":
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        content = open('index.html', 'rb').read()
+        self.wfile.write(content)
+      case "/index.html":
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        content = open('index.html', 'rb').read()
+        self.wfile.write(content)
+      case "/index.js":
+        self.send_response(200)
+        self.send_header("Content-type", "text/javascript")
+        self.end_headers()
+        content = open('index.js', 'rb').read()
+        self.wfile.write(content)
+      case "/developer-name":
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        content = {}
+        content['developerName'] = developerName
+        self.wfile.write(json.dumps(content).encode())
+      case "/healthz":
+        self.send_response(status["code"])
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        content = {}
+        content['status'] = status["healthy"]
+        self.wfile.write(json.dumps(content).encode())
+      case "/break":
+        status["code"] = 500
+        status["healthy"] = "KO"
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        content = {}
+        content['result'] = "OK"
+        self.wfile.write(json.dumps(content).encode())
+      case "/load":
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        content = {}
+        content['result'] = "OK"
+        self.wfile.write(json.dumps(content).encode())
+        for i in range(0,10000000):
+          a = math.sqrt(64*64*64*64*64)
+      case _:
+        self.send_response(404)
+    return
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+  """Handle requests in a separate thread."""
+
+if __name__ == "__main__":
+  webServer = ThreadedHTTPServer((hostName, serverPort), Handler)
+  print("Server started http://%s:%s" % (hostName, serverPort))
+
+  try:
+    webServer.serve_forever()
+  except KeyboardInterrupt:
+    pass
+
+  webServer.server_close()
+  print("Server stopped.")
